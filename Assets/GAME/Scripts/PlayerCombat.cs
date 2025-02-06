66cb9fd5517d11d91cbc85b80aa40debff818919
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-06 21:53:31 +0530

    added attack combo logic

--- Assets/GAME/Scripts/PlayerCombat.cs
+++ Assets/GAME/Scripts/PlayerCombat.cs
@@ -1,12 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] PlayerAnimationManager playerAnimationManager;
+    [SerializeField] bool canCombo = false;
+    [SerializeField] float attackComboDelay = 1f;
+    [SerializeField] string[] attackAnimationCombos;
+    [SerializeField] int currentAttackComboAnimIndex = 0;
 
     public void StartToAttack()
     {
+        if (canCombo)
+        {
+            currentAttackComboAnimIndex++;
+            playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
+
+            if(currentAttackComboAnimIndex ==  attackAnimationCombos.Length - 1)
+            {
+                currentAttackComboAnimIndex = 0;
+                canCombo = false;
+            }
+        }
+        else
+        {
+            playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
+            StartCoroutine(EnableComboAttacks(attackComboDelay));
+        }
         
-        playerAnimationManager.PlayAnyInteractiveAnimation("Sword_Attack_1", false, true);
+       
+    }
+
+    IEnumerator EnableComboAttacks(float comboDelayTime)
+    {
+        canCombo = true;
+        yield return new WaitForSeconds(comboDelayTime);
+        canCombo = false;
     }
 }
