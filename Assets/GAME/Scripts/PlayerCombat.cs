05d738ffa7b860b244a0b9702ccee52bd53f3351
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-07 14:10:23 +0530

    minor updates

--- Assets/GAME/Scripts/PlayerCombat.cs
+++ Assets/GAME/Scripts/PlayerCombat.cs
@@ -15,46 +15,11 @@ public class PlayerCombat : MonoBehaviour
 
     public void StartToAttack()
     {
-        /*if (canCombo)
-        {
-            currentAttackComboAnimIndex++;
-            playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
-
-            if(currentAttackComboAnimIndex ==  attackAnimationCombos.Length - 1)
-            {
-                currentAttackComboAnimIndex = 0;
-                canCombo = false;
-            }
-        }
-        else
-        {
-            playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
-            StartCoroutine(EnableComboAttacks(attackComboDelay));
-        }*/
-
-
-        /*if (isAttacking && canCombo)
-        {
-            currentAttackComboAnimIndex++;
-            if (currentAttackComboAnimIndex >= attackAnimCLips.Length)
-            {
-                currentAttackComboAnimIndex = 0;
-            }
-        }
-        else
-        {
-            currentAttackComboAnimIndex = 0;
-        }
-
-        playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimCLips[currentAttackComboAnimIndex].name, false, true);
-        StartCoroutine(EnableComboAttackWindow(attackComboDelay));
-        isAttacking = true;*/
-
-        if (isAttacking) return;
+        if (isAttacking) return; // cant attack if already attacking
 
         isAttacking = true;
 
-        if (canCombo)
+        if (canCombo) // if combo window is enable switch to next attack clip
         {
             canCombo = false;
             if(comboCoroutine != null)
@@ -71,12 +36,9 @@ public class PlayerCombat : MonoBehaviour
         {
             currentAttackComboAnimIndex = 0;
         }
-
-        Debug.Log("attack clip id = " + currentAttackComboAnimIndex);
         playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimClips[currentAttackComboAnimIndex].name, false, true);
         
         canCombo = true;
-        Debug.Log("Set Combo true");
         float currentAttackClipDuration = attackAnimClips[currentAttackComboAnimIndex].length;
         StartCoroutine(DisableIsAttacking(currentAttackClipDuration));
         comboCoroutine = StartCoroutine(EnableComboAttackWindow(currentAttackClipDuration + attackComboDelay));
@@ -92,11 +54,8 @@ public class PlayerCombat : MonoBehaviour
     }
 
     IEnumerator EnableComboAttackWindow(float comboDelayTime)
-    {
-        
+    { 
         yield return new WaitForSeconds(comboDelayTime);
-        canCombo = false;
-        Debug.Log("Set Combo false");
-        //currentAttackComboAnimIndex = 0;
+        canCombo = false;       
     }
 }
