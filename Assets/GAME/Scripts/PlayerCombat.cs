d9ca833722e2663ce75aeb79e5167e660afdbcfb
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-07 14:08:41 +0530

    Fixed Combo Attack logic

--- Assets/GAME/Scripts/PlayerCombat.cs
+++ Assets/GAME/Scripts/PlayerCombat.cs
@@ -6,12 +6,16 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] PlayerAnimationManager playerAnimationManager;
     [SerializeField] bool canCombo = false;
     [SerializeField] float attackComboDelay = 1f;
-    [SerializeField] string[] attackAnimationCombos;
+    [SerializeField] AnimationClip[] attackAnimClips;
     [SerializeField] int currentAttackComboAnimIndex = 0;
+    [SerializeField] private bool isAttacking = false;
+    [SerializeField] private Coroutine comboCoroutine;
+
+    
 
     public void StartToAttack()
     {
-        if (canCombo)
+        /*if (canCombo)
         {
             currentAttackComboAnimIndex++;
             playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
@@ -26,15 +30,73 @@ public class PlayerCombat : MonoBehaviour
         {
             playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimationCombos[currentAttackComboAnimIndex], false, true);
             StartCoroutine(EnableComboAttacks(attackComboDelay));
+        }*/
+
+
+        /*if (isAttacking && canCombo)
+        {
+            currentAttackComboAnimIndex++;
+            if (currentAttackComboAnimIndex >= attackAnimCLips.Length)
+            {
+                currentAttackComboAnimIndex = 0;
+            }
         }
+        else
+        {
+            currentAttackComboAnimIndex = 0;
+        }
+
+        playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimCLips[currentAttackComboAnimIndex].name, false, true);
+        StartCoroutine(EnableComboAttackWindow(attackComboDelay));
+        isAttacking = true;*/
+
+        if (isAttacking) return;
+
+        isAttacking = true;
+
+        if (canCombo)
+        {
+            canCombo = false;
+            if(comboCoroutine != null)
+            {
+                StopCoroutine(comboCoroutine);
+            }
+            currentAttackComboAnimIndex++;
+            if (currentAttackComboAnimIndex >= attackAnimClips.Length)
+            {
+                currentAttackComboAnimIndex = 0;
+            }
+        }
+        else
+        {
+            currentAttackComboAnimIndex = 0;
+        }
+
+        Debug.Log("attack clip id = " + currentAttackComboAnimIndex);
+        playerAnimationManager.PlayAnyInteractiveAnimation(attackAnimClips[currentAttackComboAnimIndex].name, false, true);
         
-       
+        canCombo = true;
+        Debug.Log("Set Combo true");
+        float currentAttackClipDuration = attackAnimClips[currentAttackComboAnimIndex].length;
+        StartCoroutine(DisableIsAttacking(currentAttackClipDuration));
+        comboCoroutine = StartCoroutine(EnableComboAttackWindow(currentAttackClipDuration + attackComboDelay));
+   
+        
+
     }
 
-    IEnumerator EnableComboAttacks(float comboDelayTime)
+    IEnumerator DisableIsAttacking(float delayTime)
     {
-        canCombo = true;
+        yield return new WaitForSeconds(delayTime);
+        isAttacking = false;
+    }
+
+    IEnumerator EnableComboAttackWindow(float comboDelayTime)
+    {
+        
         yield return new WaitForSeconds(comboDelayTime);
         canCombo = false;
+        Debug.Log("Set Combo false");
+        //currentAttackComboAnimIndex = 0;
     }
 }
