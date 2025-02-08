34ea7beade3611759fa1087e68f4e8b4d2e07cac
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-08 13:37:23 +0530

    updated enemy movement

--- Assets/GAME/Scripts/BaseEnemy.cs
+++ Assets/GAME/Scripts/BaseEnemy.cs
@@ -1,3 +1,4 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour,IDamagable
@@ -6,11 +7,62 @@ public class BaseEnemy : MonoBehaviour,IDamagable
     [SerializeField] public bool isDead = false;
     private Animator animator;
 
-    private void Start()
+    private Rigidbody enemyRigidBody;
+    [SerializeField] public float moveSpeed = 3f;
+    [SerializeField] public Vector3 moveDirection;
+    [SerializeField] public Vector3 moveVelocity;
+    [SerializeField] public Transform playerTransform;
+
+
+    private void Awake()
     {
         animator = GetComponent<Animator>();
+        enemyRigidBody = GetComponent<Rigidbody>();
+
     }
 
+    private void Start()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    public void Run()
+    {
+
+        transform.LookAt(playerTransform.position);
+
+        moveDirection = transform.forward;
+        moveVelocity = moveDirection * moveSpeed;
+        enemyRigidBody.linearVelocity = moveVelocity;
+
+        SetMovementAnimatorValues();
+
+    }
+
+    private void SetMovementAnimatorValues()
+    {
+        float x_velocityVal = Mathf.Clamp01(Mathf.Abs(moveVelocity.x));
+        float z_velocityVal = Mathf.Clamp01(Mathf.Abs(moveVelocity.z));
+
+        animator.SetFloat("X_Velocity", x_velocityVal, 0.1f, Time.deltaTime);
+        animator.SetFloat("Z_Velocity", z_velocityVal, 0.1f, Time.deltaTime);
+    }
+
+    public void ScanForPlayer()
+    {
+
+    }
+
+    public void AttackPlayer()
+    {
+
+    }
 
     public void TakeDamage(float damageAmount)
     {
