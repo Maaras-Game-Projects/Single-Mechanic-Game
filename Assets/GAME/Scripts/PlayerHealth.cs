06de8e0466c40f7a67749d0bf6f70383722c1a35
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-24 14:21:42 +0530

    added healthbar and logic

--- Assets/GAME/Scripts/PlayerHealth.cs
+++ Assets/GAME/Scripts/PlayerHealth.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
-    public float maxHealth = 500f;
+    public float totalHealth = 500f;
     public float currentHealth;
     public bool isPlayerDead = false;
+
+    [SerializeField] bool animateHealthBar = true;
+    [SerializeField] private float healthReduceSpeed = 5f;
+
+    [SerializeField] Image healthBarIMG;
 
     [SerializeField] PlayerCombat playerCombat;
     [SerializeField] PlayerAnimationManager playerAnimationManager;
 
+    public UnityEvent OnPlayerTakeDamage;
+    public UnityEvent OnPlayerDead;
+
+
     private void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = totalHealth;
+        SetHealthBarSizeToTotalHealth();
+        
+    }
+
+    private void SetHealthBarSizeToTotalHealth()
+    {
+        Vector2 healthBarSize = healthBarIMG.rectTransform.sizeDelta;
+        healthBarSize.x = totalHealth;
+        healthBarIMG.rectTransform.sizeDelta = healthBarSize;
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBarIMG.fillAmount = currentHealth / totalHealth;
+    }
+
+    private void UpdateHealthWithAnimation()
+    {
+        float targetHealth = currentHealth / totalHealth;
+
+        healthBarIMG.fillAmount = Mathf.MoveTowards(healthBarIMG.fillAmount, targetHealth, Time.deltaTime * healthReduceSpeed);
     }
 
     public void TakeDamage(float DamageVal)
@@ -24,6 +56,17 @@ public class PlayerHealth : MonoBehaviour
 
         playerAnimationManager.PlayAnyInteractiveAnimation("Hit_Front", true,true);
 
+        if(animateHealthBar)
+        {
+            UpdateHealthWithAnimation();
+        }
+        else
+        {
+            UpdateHealthBar();
+        }
+
+        OnPlayerTakeDamage?.Invoke();
+
         if (currentHealth <= 0)
         {
            
@@ -31,6 +74,8 @@ public class PlayerHealth : MonoBehaviour
             playerCombat.playerSword.EnableDisableSwordCollider(false);
             playerCombat.playerSword.SetSwordRotationValueAtPlayerDeath();
             isPlayerDead = true;
+
+            OnPlayerDead?.Invoke();
         }
     }
 }
