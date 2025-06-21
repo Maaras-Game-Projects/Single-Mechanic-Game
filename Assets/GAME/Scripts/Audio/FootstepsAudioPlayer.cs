9cb3e8dd7a1c79793ae2d12864e947458f4a945d
Maara <92599683+Maara12@users.noreply.github.com>
2025-06-21 14:43:32 +0530

    updated  Footsteps audio logic

--- Assets/GAME/Scripts/Audio/FootstepsAudioPlayer.cs
+++ Assets/GAME/Scripts/Audio/FootstepsAudioPlayer.cs
@@ -4,22 +4,36 @@ public class FootstepsAudioPlayer : MonoBehaviour
 {
     [SerializeField] Animator animator;
 
+    [SerializeField] PlayerAnimationManager playerAnimationManager;
+    [SerializeField] PlayerLocomotion playerLocomotion;
+    [SerializeField] PlayerCombat playerCombat;
+
     [SerializeField] string footStepParamString;
     [SerializeField] float lastFootStep = 0;
 
-    [SerializeField] AudioClip[] audioClips;
+    [SerializeField] AudioClipData[] audioClips;
 
     void Update()
     {
         if (animator == null) return;
+
+        if (playerAnimationManager.inAnimActionStatus) return;
+        if (playerLocomotion.isDodging) return;
+        if (!playerLocomotion.isGrounded) return;
+        if (playerAnimationManager.rootMotionUseStatus && !playerCombat.isBlocking) return;
 
         float currentFootstep = animator.GetFloat(footStepParamString);
 
         if (lastFootStep < 0 && currentFootstep > 0 || lastFootStep > 0 && currentFootstep < 0)
         {
             int randomIndex = Random.Range(0, audioClips.Length);
-            AudioManager_STN.instance.PlayAudio_SFX_General(audioClips[randomIndex], transform.position, 1
-            , 0);
+            AudioClip clip = audioClips[randomIndex].clip;
+            Vector3 position = audioClips[randomIndex].position;
+            float volume = audioClips[randomIndex].volume;
+            float pitch = audioClips[randomIndex].pitch;
+            float spatialBlend = audioClips[randomIndex].spatialBlendVal;
+            AudioManager_STN.instance.PlayAudio_SFX_General(clip, position, volume,
+            pitch, spatialBlend);
         }
 
         lastFootStep = currentFootstep;
